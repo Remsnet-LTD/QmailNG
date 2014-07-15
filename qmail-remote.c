9dcd7587bfd157724b4460c1a808a8d3a516907a
Ismael Luceno <ismael@iodev.co.uk>
2014-07-14 21:19:49 -0300

    Import qmail-ldap 20030501

--- qmail-remote.c
+++ qmail-remote.c
@@ -354,8 +354,7 @@ void smtp()
   substdio_flush(&smtpto);
 
   code = smtpcode();
-  if (code >= 500) quit("DConnected to "," but my name was rejected");
-  if (code != 250){
+  if (code != 250) {
    substdio_puts(&smtpto,"HELO ");
    substdio_put(&smtpto,helohost.s,helohost.len);
    substdio_puts(&smtpto,"\r\n");
